51944b9235fa353c46a189f89791a13fb4e41360
Vladislav Voloshanovskiy <voloshanowky@yandex.ru>
2024-10-14 19:38:11 +0400

    finished tast 1-5

---
+++ src/app/housing-location/housing-location.component.ts
@@ -0,0 +1,28 @@
+import { Component, Input } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { Housinglocation } from '../housinglocation';
+import { RouterModule } from '@angular/router';
+@Component({
+  selector: "app-housing-location",
+  standalone: true,
+  imports: [CommonModule, RouterModule],
+  template: `
+    <section class="listing">
+      <img
+        class="listing-photo"
+        [src]="housinglocation.photo"
+        alt="photo
+      {{ housinglocation.name }}"
+      />
+      <h2 class="listing-heading">{{ housinglocation.name }}</h2>
+      <p class="listing-location">
+        {{ housinglocation.city }}, {{ housinglocation.state }}
+      </p>
+      <a [routerLink]="['/details', housinglocation.id]">Learn more</a>
+    </section>
+  `,
+  styleUrls: ["./housing-location.component.css"],
+})
+export class HousingLocationComponent {
+  @Input() housinglocation!: Housinglocation;
+}
